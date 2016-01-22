19a9dd41f52bc38344c103a2e1f2d70ec8f0fe35
Andry Sobolev <allmywebwork@gmail.com>
2016-01-22 16:18:31 +0200

    Drupal core update and minor fix

--- sxd/ses.php
+++ sxd/ses.php
@@ -1,4 +1,31 @@
 <?php
 $SES = array (
+  'dd2991e8e06271c62b4c237ec816d8db' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'webwork.mysql.ukraine.com.ua',
+      'my_port' => 3306,
+      'my_user' => 'webwork_absolut',
+      'my_pass' => 'armgcjap',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1453466002,
+    'lng' => 'ru',
+  ),
 );
 ?>
